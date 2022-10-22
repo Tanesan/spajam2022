f912301b49ed62747cc2296664d73eda58d34dc6
Tanesan <keito.12.10@icloud.com>
2022-10-23 08:42:00 +0900

    背景を白に変更

--- lib/ui/pages/room_enter_page.dart
+++ lib/ui/pages/room_enter_page.dart
@@ -14,21 +14,24 @@ class _RoomEnterPageState extends State<RoomEnterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         body: Column(
-      children: [
-        HeaderCustomized(title: "ルーム参加"),
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-          child: TextFieldCustom(hintText: "ユーザー名を入力", onPressed: () => null),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-          child: TextFieldCustom(
-              suffix: "確定",
-              hintText: "ルームIDを入力",
-              onPressed: () => Navigator.of(context).pushNamed('/roomroby')),
-        ),
-      ],
-    ));
+          children: [
+            HeaderCustomized(title: "ルーム参加"),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+              child:
+                  TextFieldCustom(hintText: "ユーザー名を入力", onPressed: () => null),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+              child: TextFieldCustom(
+                  suffix: "確定",
+                  hintText: "ルームIDを入力",
+                  onPressed: () =>
+                      Navigator.of(context).pushNamed('/roomroby')),
+            ),
+          ],
+        ));
   }
 }
--- lib/ui/pages/room_roby_page.dart
+++ lib/ui/pages/room_roby_page.dart
@@ -15,91 +15,93 @@ class _RoomRobyPageState extends State<RoomRobyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         body: Column(
-      children: [
-        HeaderCustomized(title: "ルーム"),
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-          child: Column(
-            children: [
-              Column(
+          children: [
+            HeaderCustomized(title: "ルーム"),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+              child: Column(
                 children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text("ルームID", textAlign: TextAlign.left),
+                          ],
+                        ),
+                      ),
+                      TextFieldCustom(
+                          suffix: "コピー",
+                          hintText: "ルームID",
+                          InputController:
+                              TextEditingController(text: "a43yf89"),
+                          onPressed: () => null),
+                    ],
+                  ),
                   Padding(
-                    padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                    padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
+                    child: Column(
                       children: [
-                        Text("ルームID", textAlign: TextAlign.left),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text("参加者", textAlign: TextAlign.left),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                          child: ListView.builder(
+                              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                              itemCount: 2,
+                              shrinkWrap: true,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                                    child: TextFieldCustom(
+                                      hintText: "ルームID",
+                                      onPressed: () {},
+                                      InputController:
+                                          TextEditingController(text: "1234"),
+                                      prefixIcon: CircleAvatar(
+                                          backgroundColor: Colors.transparent,
+                                          child: Constants.avatars[index]),
+                                    ));
+                              }),
+                        ),
                       ],
                     ),
                   ),
-                  TextFieldCustom(
-                      suffix: "コピー",
-                      hintText: "ルームID",
-                      InputController: TextEditingController(text: "a43yf89"),
-                      onPressed: () => null),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text("参加者", textAlign: TextAlign.left),
-                        ],
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 150, 0, 0),
+                    child: OutlinedButton(
+                      child: Container(
+                          width: 190,
+                          height: 40,
+                          child: Center(child: Text('開始する'))),
+                      style: OutlinedButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Color(0xffCF9090),
+                        side: BorderSide(color: Color(0xffCF9090)),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: ListView.builder(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                          itemCount: 2,
-                          shrinkWrap: true,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Padding(
-                                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                                child: TextFieldCustom(
-                                  hintText: "ルームID",
-                                  onPressed: () {},
-                                  InputController:
-                                      TextEditingController(text: "1234"),
-                                  prefixIcon: CircleAvatar(
-                                      backgroundColor: Colors.transparent,
-                                      child: Constants.avatars[index]),
-                                ));
-                          }),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 150, 0, 0),
-                child: OutlinedButton(
-                  child: Container(
-                      width: 190,
-                      height: 40,
-                      child: Center(child: Text('開始する'))),
-                  style: OutlinedButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Color(0xffCF9090),
-                    side: BorderSide(color: Color(0xffCF9090)),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/home');
+                      },
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/home');
-                  },
-                ),
+                ],
               ),
-            ],
-          ),
-        )
-      ],
-    ));
+            )
+          ],
+        ));
   }
 }
